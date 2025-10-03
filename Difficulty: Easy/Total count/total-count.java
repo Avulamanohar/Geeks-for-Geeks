a7e5cf043657152e1fb99f02983a4ade4b9045c2
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2025-10-03 19:32:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Total count/total-count.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+
+class Solution {
+    int totalCount(int k, int[] arr) {
+        // code here
+        int count=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            count+=arr[i]/k;
+            if(arr[i]%k!=0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
