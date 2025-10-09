b62a44d073d11173dc2731d7370191ed8e594c89
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2025-10-09 16:01:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Peak element/peak-element.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int peakElement(int[] arr) {
+    int n=arr.length;
+    if(n==1)
+    {
+        return 0;
+    }
+    if(Integer.MIN_VALUE<arr[0]&&arr[0]>arr[1])
+    {
+        return 0;
+    }
+    for(int i=1;i<n-1;i++)
+    {
+        if(arr[i]>arr[i-1]&&arr[i]>arr[i+1])
+        {
+            return i;
+        }
+    }
+    if(arr[n-1]>Integer.MIN_VALUE&&arr[n-1]>arr[n-2])
+    {
+        return n-1;
+    }
+    return -1;
+        
+    }
+}
